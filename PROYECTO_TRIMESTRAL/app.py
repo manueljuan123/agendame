cb2c93c34f81ce54f660c91b6a083135273ce431
Juan Yate Méndez <juanmanuelyatemendez@gmail.com>
2021-03-17 10:36:44 -0500

    Se agregó interfaz login

--- PROYECTO_TRIMESTRAL/app.py
+++ PROYECTO_TRIMESTRAL/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_flask
+from flask import Flask, render_template, request, redirect #url_flask
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -13,20 +13,13 @@ mysql = MySQL(app)
 app.secret_key = 'mysecretkey'
 
 @app.route('/')
-def Index():
-    cur=mysql.connection.cursor()
-    cur.execute('SELECT * FROM usuarios')
-    data = cur.fetchall()
-    cur.close()
-    return render_template('index.html', contacts=data)
+def index():
+    #cur=mysql.connection.cursor()
+    #cur.execute('SELECT * FROM usuarios')
+    #data = cur.fetchall()
+    #cur.close()
+    return render_template('index.html')
 
-@app.route('/registrar_usuario', methods=['POST'])
-def reg_usuario():
-    if request.method == 'POST':
-        nombre = request.form['nombre']
-        apellido = request.form['apellido']
-        edad = request.form['apellido']
-        ocupacion = request.form['ocupacion']
-        email = request.form['email']
-        contraseña = request.form['contraseña']
+if __name__== "__main__":
+    app.run(debug=True)
 
