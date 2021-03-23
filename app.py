bfc99fd114dfe50f348772df2a7ce54e52e7f377
manueljuan123 <jmyate5@misena.edu.co>
2021-03-23 12:17:02 -0500

    código html y py
    Se agregó código de python y uno nuevo de html, sin embargo, aún no sirve la función login para ingresar al html eventos

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, redirect #url_flask
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = ''
-app.config['MYSQL_USER'] = ''
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DATABASE'] = ''
+app.config['MYSQL_DB'] = 'agendate'
 
 mysql = MySQL(app)
 
@@ -14,11 +15,30 @@ app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def index():
-    #cur=mysql.connection.cursor()
-    #cur.execute('SELECT * FROM usuarios')
-    #data = cur.fetchall()
-    #cur.close()
-    return render_template('index.html')
+    cur=mysql.connection.cursor()
+    cur.execute('SELECT * FROM usuario')
+    data = cur.fetchall()
+    cur.close()
+    return render_template('index.html', usuarios = data) 
+
+
+
+@app.route('/login', methods=["POST"])
+def login(cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx):
+    email=request.form['email']
+    contrasena=request.form['contrasena']
+
+    try:
+        cur=mysql.connection.cursor()
+        cur.execute(f'SELECT * FROM usuario WHERE email = {email}')
+        data = cur.fetchall()
+        cur.close()
+        return render_template('eventos.html', email = data[0])
+    except:
+        flash ("El usuario o la contraseña no son correctos. Revise o regístrese en la parte inferior, por favor")
+        return render_template('index.html')
+        
+
 
 if __name__== "__main__":
     app.run(debug=True)
